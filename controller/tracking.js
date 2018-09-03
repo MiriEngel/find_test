c1006ef45ff50364834dee05556372cb284dd9b9
Miri Engel <miri.engel10@gmail.com>
2018-09-04 00:41:08 +0300

    hk

--- controller/tracking.js
+++ controller/tracking.js
@@ -12,7 +12,10 @@ const options = {
 // setTimeout(() => {
 //   sendMsgClient({data:'testttttttttttttt'});  
 // }, 30000);
-
+(() =>{
+let data = '78780d010353701091381687001689040d0a';
+          //  787805010001d9dc0d0a
+})()
 sendMsgClient({data:'testttttttttttttt'});
 //protocol:gt06	 port:5023
 var server = gps.server(options,function(device,connection){
@@ -26,12 +29,12 @@ var server = gps.server(options,function(device,connection){
 
     device.on("login_request",function(device_id,msg_parts){
 
-        console.log('Hey! I want to start transmiting my position. Please accept me. My name is '+device_id);
+       // console.log('Hey! I want to start transmiting my position. Please accept me. My name is '+device_id);
 
         //this.login_authorized(true); 
 
         console.log("Ok, "+device_id+", you're accepted!");
-
+console.log(msg_parts);
     });
     
 
