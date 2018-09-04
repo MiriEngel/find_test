3372698b6dbdba063a5e503d680ab27086d2c1fa
Miri Engel <miri.engel10@gmail.com>
2018-09-04 09:53:43 +0300

    sd

--- controller/tracking.js
+++ controller/tracking.js
@@ -29,11 +29,11 @@ var server = gps.server(options, function (device, connection) {
 
     device.on("login_request", function (device_id, msg_parts) {
 
-         console.log('Hey! I want to start transmiting my position. Please accept me. My name is '+device_id);
+         //console.log('Hey! I want to start transmiting my position. Please accept me. My name is '+device_id);
 
         this.login_authorized(true); 
 
-        //console.log("Ok, " + device_id + ", you're accepted!");
+        console.log("Ok, " + device_id + ", you're accepted!");
         //console.log(msg_parts);
     });
 
@@ -45,7 +45,7 @@ var server = gps.server(options, function (device, connection) {
         //Look what informations the device sends to you (maybe velocity, gas level, etc)
         //console.log(data);
         sendMsgClient({ data });
-        //return data;
+        return data;
 
     });
 
